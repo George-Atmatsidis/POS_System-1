fef23df4c39e4efd6e55464531f052cbf09d9c90
cadepowers99 <cadepowers99@gmail.com>
2020-12-07 12:15:26 -0600

    [CADE]

--- frontEnd/pythonApp/server.py
+++ frontEnd/pythonApp/server.py
@@ -26,11 +26,7 @@ class server:
             try:
                 response = json.loads(response.content)
             except json.decoder.JSONDecodeError as e:
-                if (e.msg == "Extra data error"): #process multiple data entries
-                    response = []
-                    for jsonObj in response.content:
-                        jsondict = json.loads(jsonObj)
-                        response.append(jsondict)
+                print("no content")
         else:
             if(response.status_code == 501):
                 print("NOT IMPLEMENTED")
@@ -45,34 +41,25 @@ class server:
             try:
                 response = json.loads(response.content)
             except json.decoder.JSONDecodeError as e:
-                if (e.msg == "Extra data error"): #process multiple data entries
-                    response = []
-                    for jsonObj in response.content:
-                        jsondict = json.loads(jsonObj)
-                        response.append(jsondict)
-        else:
+                print("no content")
             if(response.status_code == 501):
                 print("NOT IMPLEMENTED")
                 sleep(2)
             else:
-                raise ConnectionError("GET request failed")
+                raise ConnectionError("PUT request failed")
         return response
 
     def post(self, route, data):
         response = self.session.post(self.host+route, data=data, headers=self.header)
         if (response):
             try:
-                response = json.loads(response.content)
+                response = json.loads(response.content) #multiple entries will be read into arrays
             except json.decoder.JSONDecodeError as e:
-                if (e.msg == "Extra data error"): #process multiple data entries
-                    response = []
-                    for jsonObj in response.content:
-                        jsondict = json.loads(jsonObj)
-                        response.append(jsondict)
+                print("no content")
         else:
             if(response.status_code == 501):
                 print("NOT IMPLEMENTED")
                 sleep(2)
             else:
-                raise ConnectionError("GET request failed")
+                raise ConnectionError("POST request failed")
         return response
