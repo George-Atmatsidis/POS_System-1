f0452fb2b44be49dca85fd4e6ee61d529e395384
pcase74 <60786585+pcase74@users.noreply.github.com>
2020-12-18 11:23:07 -0600

    Add files via upload

--- backEnd/app.js
+++ backEnd/app.js
@@ -103,7 +103,21 @@ app.post('/register', async (req, res) => {
 
 app.get('/inventoryManagement/partsManagement', async (req, res) => {  // DO it for all Routes asap
     const {PartNumberPattern} = req.body;
-    res.json({PartNumber: "45125A", Description: "Gear", Quantity: 2, Price: 120.00}); //Send product Info
+    res.json({ parts: [
+            {
+                PartNumber: "45125A", 
+                Description: "Gear", 
+                Quantity: 2, 
+                Price: 120.00
+            },
+            {
+                PartNumber: "523125A", 
+                Description: "Tire", 
+                Quantity: 1, 
+                Price: 200.00
+            }
+        ]
+    }); //Send product Info
 })
 
 app.put('/inventoryManagement/partsManagement/add', async (req, res) => {
@@ -120,7 +134,21 @@ app.put('/inventoryManagement/partsManagement/add', async (req, res) => {
 
 app.get('/accountsRecievable', async (req, res) =>{
     //Communicate with DB and get list of Accounts.
-    res.json({AccountNumber: 001, CompanyName: "RedStone", AmountOwed: 1200.23, DateDue: "10/5/20"}); //Send Stubbed list of accounts
+    res.json({ accounts: [
+            {   
+                AccountNumber: 001, 
+                CompanyName: "RedStone", 
+                AmountOwed: 1200.23, 
+                DateDue: "10/5/20"
+            },
+            {
+                AccountNumber: 002,
+                CompanyName: "Red Deer",
+                AmountOwed: 123.45,
+                DateDue: "4/20/20"
+            }
+        ]
+    }); //Send Stubbed list of accounts
 })
 
 app.post('accountsRecievable/paid', async (req, res) =>{
@@ -145,12 +173,17 @@ app.post('/inventoryManagement/partsManagement/detailed', async (req, res) =>{
 })
 
 app.get('/inventoryManagement/classManagement', async (req, res) =>{
-    res.json({ClassID: 001,
+    res.json({ classes: [{ClassID: 001,
         ClassDescr: "Gears",
         Margin1: 1.2,
         Margin2: 1.6,
         Margin3: 2.0
-    }); //send stubbed class
+    }, {ClassID: 002,
+        ClassDescr: "Tires",
+        Margin1: 1.5,
+        Margin2: 1.9,
+        Margin3: 2.3
+    }]}); //send stubbed class
 })
 
 app.post('/inventoryManagement/classmanagement/update', async (req, res) =>{
@@ -161,16 +194,28 @@ app.post('/inventoryManagement/classmanagement/update', async (req, res) =>{
 
 app.get('/customerManagement', async (req,res) =>{
     const {Name} = req.body;
-    res.json({ID: 001,
-        Name: "Redstone",
-        Addr: "1234 Redstone St. Ruelle, AR, 72451",
-        Phone: "501-405-5124"
-    }); //Send stubbed Data
+    res.json({customers: [
+        {
+            ID: 001,
+            Name: "Redstone",
+            Addr: "1234 Redstone St. Ruelle, AR, 72451",
+            Phone: "501-405-5124"
+        },
+        {
+            ID: 002,
+            Name: "Red Deer",
+            Addr: "321 Red Deer Blvd. Jonesboro, AR, 72401",
+            Phone: "870-802-1234"
+        }
+    ]}); //Send stubbed Data
 })
 
 app.get('/customerManagement/detailed', async (req,res) =>{
     const {ID} = req.body;
-    res.json({BillingAddr: "POBox 203 Ruelle, AR, 72451",
+    res.json({Name: "Redstone",
+        Addr: "1234 Redstone St. Ruelle, AR, 72451",
+        Phone: "501-405-5124",
+        BillingAddr: "POBox 203 Ruelle, AR, 72451",
         ShippingAddr: "ATTN: Robby 1234 Redstone St. Ruelle, AR, 72451",
         CityTax: 0.05,
         StateTax: 0.1,
@@ -196,30 +241,81 @@ app.put('/customerManagement/add', async (req,res) =>{
 })
 
 app.get('/employeeManagement', async(req,res) =>{
-    res.sendStatus(501); //Not Implemented
+    const {Name} = req.body;
+    res.json({employees: [
+        {
+            ID: 1001,
+            Name: "Cade Powers",
+            Addr: "2134 Red Wold, Jonesboro, AR, 72142",
+            Phone: "321-654-0987"
+        },
+        {
+            ID: 1002,
+            Name: "Patrick Case",
+            Addr: "1234 Qwertrt, Jonesboro, AR, 72401",
+            Phone: "456-789-7890"
+        },
+        {
+            ID: 1003,
+            Name: "Fady Farag",
+            Addr: "1345 Johnson, Jonesboro, AR, 72401",
+            Phone: "759-186-4451"
+        },
+        {
+            ID: 1004,
+            Name: "Ryouji Sezai",
+            Addr: "2560 E Johnson, Jonesboro, AR, 72401",
+            Phone: "758-458-7894"
+        }
+    ]});
 })
 
 app.get('/employeeManagement/detailed', async(req,res) =>{
-    res.sendStatus(501); //Not Implemented
+    const {ID} = req.body;
+    res.json({
+        Name: "Cade Powers",
+        Addr: "2134 Red Wold, Jonesboro, AR, 72142",
+        Phone: "321-654-0987",
+        Username: "cade",
+        Role: "Admin"
+    }); //Send stubbed data
 })
 
 app.post('/employeeManagement/add', async(req,res) =>{
-    res.sendStatus(501); //Not Implemented
+    const {name, 
+        userName, 
+        password, 
+        empNo, 
+        address, 
+        phone, 
+        role
+    } = req.body;
+    res.sendStatus(200); //Not Implemented
 })
 
 app.get('/partsCounter/invoice', async(req,res) =>{
     const {CustomerName} = req.body;
-    res.json({ID: 30001,
-        Customer: "Redstone",
-        Date: "10/07/2020",
-        FirstPart: "101-45214-000",
-        Total: 1000.23
-    }); //Stend stubbed data
+    res.json({ invoices: [
+        {
+            ID: 30001,
+            Customer: "Redstone",
+            Date: "10/07/2020",
+            FirstPart: "101-45214-000",
+            Total: 1000.23
+        },
+        {
+            ID: 30002,
+            Customer: "Redstone",
+            Date: "11/07/2020",
+            FirstPart: "101-45214-000",
+            Total: 1000.23
+        }
+    ]}); //Stend stubbed data
 })
 
 app.post('/partsCounter/invoice/add', async(req,res) =>{
     
-    res.sendStatus(501); //Not Implemented
+    res.sendStatus(200); //Not Implemented
 })
 
 app.put('/partsCounter/workOrder/update', async(req,res) =>{
